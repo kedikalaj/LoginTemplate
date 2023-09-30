d852178bc7c65dba276ab7935fd6137a87a236c9
Kedi Kalaj <93157449+kedikalaj@users.noreply.github.com>
2023-09-30 02:08:26 +0200

    Added the basic models

---
+++ LoginTemplate/Model/Authentication/RegisterUserViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LoginTemplate.Model.Authentication
+{
+    public class RegisterUserViewModel
+    {
+        /// <summary>
+        /// Gets or sets the username of the user
+        /// </summary>
+        [Required(ErrorMessage = "The username is required")]
+        public string Username { get; set; }
+        /// <summary>
+        /// Gets or sets the username of the user
+        /// </summary>
+        [EmailAddress]
+        [Required(ErrorMessage = "The email is required")]
+        public string Email { get; set; }
+        /// <summary>
+        /// Gets or sets the username of the user
+        /// </summary>
+        [Required(ErrorMessage = "The password is required")]
+        public string Password { get; set; }
+
+    }
+}
---
+++ LoginTemplate/Model/Authentication/Response.cs
@@ -0,0 +1,14 @@
+﻿namespace LoginTemplate.Model.Authentication
+{
+    public class Response
+    {
+        /// <summary>
+        /// Gets or sets the status of the response
+        /// </summary>
+        public string? Status { get; set; }
+        /// <summary>
+        /// Gets or sets the message of the response
+        /// </summary>
+        public string? Message { get; set; }
+    }
+}
